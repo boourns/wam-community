c25403c5c9c93bea811658308365691437f2b7a4
Tom Burns <tom@burns.ca>
2022-10-20 14:46:56 -0400

    server: serve plugins

--- example/server.js
+++ example/server.js
@@ -1,12 +1,19 @@
 import express from 'express'
+
 import {Plugins, Categories} from '../dist/index.js';
 
 const app = express()
 const port = 3500
 
 app.get('/', (req, res) => {
-    res.send("The following plugins are available: ", Plugins.map(p => p.name))
+const body = `
+The following plugins are available:
+${Plugins.map(p => `<a href="/plugins/${p.path}/index.js">${p.name}</a>`).join("<br />")}
+`
+    res.status(200).send(body)
 })
+
+app.use("/plugins", express.static('../dist/plugins'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
