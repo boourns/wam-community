01edca4195e036c3c2050922a5d1c85cba3ac55d
micbuffa <micbuffa@gmail.com>
2024-02-26 16:47:00 +0100

    added newly patched sdk-parammgr

--- plugins/wimmics/utils/sdk-parammgr/src/sdk/src/WamParameterInfo.d.ts
+++
@@ -1,5 +0,0 @@
-import { WamParameterInfo } from '@webaudiomodules/api';
-
-declare const getWamParameterInfo: (moduleId?: string) => typeof WamParameterInfo;
-
-export default getWamParameterInfo;
--- plugins/wimmics/utils/sdk-parammgr/src/sdk/src/WamProcessor.d.ts
+++
@@ -1,5 +0,0 @@
-import { WamProcessor } from './types';
-
-declare const getWamProcessor: (moduleId?: string, dependencies?: { RingBuffer: string; WamEventRingBuffer: string; WamParameter: string; WamParameterInterpolator: string; }) => typeof WamProcessor;
-
-export default getWamProcessor;
